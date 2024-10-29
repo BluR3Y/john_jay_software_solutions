795c1541c7c59c2cc52e12bd38abd8019f92bd97
Rey Flores <69604218+BluR3Y@users.noreply.github.com>
2024-10-29 04:24:19 -0400

    Developed process to populate the department of projects

--- cayuse_feedback_modifier/src/index.py
+++ cayuse_feedback_modifier/src/index.py
@@ -13,6 +13,7 @@ import configs.openai_config as openai_config
 import sheets.members as members
 import sheets.attachments as attachments
 import sheets.awards as awards
+import sheets.proposals as proposals
 
 class FeedBackModifier:
     
@@ -32,7 +33,7 @@ class FeedBackModifier:
     def save_excel_changes(self, as_copy = True, index=False):
         try:
             save_path = os.getenv('COPY_SAVE_PATH') if as_copy and os.getenv('COPY_SAVE_PATH') else os.path.dirname(self.filepath)
-            file_name = f"{os.path.splitext(os.path.basename(self.filepath))[0]}{' - modified_copy' if as_copy else ''}.xlsx"
+            file_name = f"{os.path.splitext(os.path.basename(self.filepath))[0]}{f" - modified_copy_{datetime.datetime.today().strftime('%m-%d-%Y')}" if as_copy else ''}.xlsx"
 
             # Use ExcelWriter to write multiple sheets back into the Excel file
             with pd.ExcelWriter(os.path.join(save_path, file_name), engine='openpyxl', mode='w') as writer:
@@ -41,7 +42,7 @@ class FeedBackModifier:
                     df_sheet.to_excel(writer, sheet_name=sheet_name, index=index)
 
             # # Log the modifications
-            with open(save_path + '/excel_modifier_logger.json', 'w') as json_file:
+            with open(save_path + f"/excel_modifier_logger_{datetime.datetime.today().strftime('%m-%d-%Y')}.json", 'w') as json_file:
                 json.dump(self.logger, json_file)
         except Exception as e:
             print(f"Error saving Excel file: {e}")
@@ -62,6 +63,8 @@ FeedBackModifier.get_missing_project_attachments = attachments.missing_project_a
 FeedBackModifier.get_project_info = attachments.retrieve_project_info
 FeedBackModifier.populate_db_award_discipline = awards.populate_db_discipline
 FeedBackModifier.populate_template_award_discipline = awards.populate_template_discipline
+FeedBackModifier.populate_template_award_department = awards.populate_template_department
+FeedBackModifier.populate_template_proposal_department = proposals.populate_template_department
 
 # Testing the Modifier class:
 if __name__ == "__main__":
--- cayuse_feedback_modifier/src/sheets/awards.py
+++ cayuse_feedback_modifier/src/sheets/awards.py
@@ -37,10 +37,10 @@ def populate_db_discipline(self):
             if regex_match:
                 discipline = regex_match.group(1)
             else:
-                sheet_logger[f"{primary}-{secondary}-{tertiary}"] = "Project is assigned an unusual 'Discipline' value in the imported file."
+                sheet_logger[f"{primary}-{secondary}-{tertiary}"] = f"The value '{discipline}' for the Discipline of the Project is unusually formatted"
         else:
             discipline = None
-            sheet_logger[f"{primary}-{secondary}-{tertiary}"] = "Project is missing 'Discipline' value in the imported file."
+            sheet_logger[f"{primary}-{secondary}-{tertiary}"] = "Project is missing Discipline value in the imported file."
             continue
 
         correlating_item = None
@@ -76,7 +76,7 @@ def populate_db_discipline(self):
             for key in missing_keys:
                 sheet_logger[f"{key}:database"] = "Record does not exist in the database."
             
-        print(f"Database modifications are {round(index/len(project_disciplines) * 100)}% complete")
+        print(f"Process is {round(index/len(project_disciplines) * 100)}% complete")
 
     self.append_process_logs(SHEET_NAME, {
         "populate_db_discipline": {
@@ -85,16 +85,23 @@ def populate_db_discipline(self):
     } })
 
 def populate_template_discipline(self):
+    # Retrieve the content of the awards sheet
     award_sheet_content = self.df[SHEET_NAME]
     # Acquire all the rf_ids from the award sheet in the excel file
     award_query_ids = award_sheet_content['proposalLegacyNumber'].tolist()
     sheet_logger = dict()
+
+    # Retrieve the disciplines from the table LU_Discipline in the database
+    discipline_query = "SELECT Name FROM LU_Discipline;"
+    discipline_result = self.execute_query(discipline_query)
+    valid_disciplines = [value['Name'] for value in discipline_result]
     
     last_index = 0
+    batch_limit = 40
     # Loop while index has not reached the length of the list
     while last_index <= len(award_query_ids):
         # The new end index will be the previous value plus 40, i.e, the limit of the list that will store the ids
-        new_end = last_index + 40
+        new_end = last_index + batch_limit
         batch_ids = award_query_ids[last_index:new_end]
         # Assign the value of last_index to be that of new_end
         last_index = new_end
@@ -104,12 +111,7 @@ def populate_template_discipline(self):
         search_result = self.execute_query(search_query, batch_ids)
         project_disciplines = { project['Grant_ID']:project['Discipline'] for project in search_result }
 
-        # Retrieve the disciplines from the table LU_Discipline in the database
-        discipline_query = "SELECT Name FROM LU_Discipline;"
-        discipline_result = self.execute_query(discipline_query)
-        valid_disciplines = [value['Name'] for value in discipline_result]
-
-        # Loop through every rf_id in the batch
+        # Loop through every grant_id in the batch
         for id in batch_ids:
             # Check if the database returned a record with the id
             if id in project_disciplines:
@@ -122,15 +124,75 @@ def populate_template_discipline(self):
                         # Assign the 'Discipline' property of the row with the id the updated value
                         award_sheet_content.loc[file_record_index, 'Discipline'] = project_disciplines[id]
                     else:
-                        sheet_logger[f"{id}:discipline"] = "The record's 'Discipline' value may be invalid"
+                        sheet_logger[f"{id}:discipline"] = f"The value '{project_disciplines[id]}' for the Discipline of the record may be invalid."
                 else:
-                    sheet_logger[f"{id}:discipline"] = "The record does not have a value assigned for the 'Discipline' column"
+                    sheet_logger[f"{id}:discipline"] = "The record does not have a value assigned for the 'Discipline' column."
             else:
-                sheet_logger[f"{id}:database"] = "RF_Account is not associated with any record in the database."
+                sheet_logger[f"{id}:database"] = "Id is not associated with any record in the database."
+        
+        print(f"Process is {round(last_index/len(award_query_ids) * 100)}% complete")
 
     self.append_process_logs(SHEET_NAME, {
         "populate_template_discipline": {
             "description": "Process populates the 'Discipline' column of the awards in the template document with the value the record is assigned in the Microsoft Access database. Additionally, the process also catches various types of issues found in the data from multiple sources such as the template file and the database and logs them.",
+            "logs": sheet_logger
+        }
+    })
+
+def populate_template_department(self):
+    # Retrieve the content of the proposals sheet
+    proposal_sheet_content = self.df[SHEET_NAME]
+    # Acquire all the rf_ids from the award sheet in the excel file
+    proposal_query_ids = proposal_sheet_content['proposalLegacyNumber'].tolist()
+    sheet_logger = dict()
+
+    # Retrieve the departments from the table LU_Department in the database
+    department_query = "SELECT LU_Department AS Name FROM LU_Department"
+    department_result = self.execute_query(department_query)
+    valid_departments = [value['Name'] for value in department_result]
+
+    last_index = 0
+    batch_limit = 40
+    # Loop while index has not reached the length of the list
+    while last_index <= len(proposal_query_ids):
+        # The new end index will be the previous value plus 40, i.e, the limit of the list that will store the ids
+        new_end = last_index + batch_limit
+        batch_ids = proposal_query_ids[last_index:new_end]
+        # Assign the value of last_index to be that of new_end
+        last_index = new_end
+
+        # Retrieve the Grant_ID and the Primary_Dept value of the records in the batch_ids list
+        search_query = f"""
+            SELECT Grant_ID, Primary_Dept
+            FROM grants
+            WHERE Grant_ID IN ({','.join(['?' for _ in batch_ids])})
+        """
+        search_result = self.execute_query(search_query, batch_ids)
+        project_departments = { project['Grant_ID']:project['Primary_Dept'] for project in search_result }
+
+        # Loop through every grant_id in the batch
+        for id in batch_ids:
+            # Check if the database returned a record with the id
+            if id in project_departments:
+                # Check that the database did not return an empty value for the department of the current record
+                if project_departments[id]:
+                    if project_departments[id] in valid_departments:
+                        # Retrieve the index of the row with the current id in the template file
+                        file_record_index = proposal_sheet_content.index[proposal_sheet_content['proposalLegacyNumber'] == id]
+                        # Assign the 'Department' property of the row with the id the updated value
+                        proposal_sheet_content.loc[file_record_index, 'Admin Unit'] = project_departments[id]
+                    else:
+                        sheet_logger[f"{id}:department"] = f"The value '{project_departments[id]}' for the department of the record may be invalid."
+                else:
+                    sheet_logger[f"{id}:department"] = "The record does not have a value assigned for the department column"
+            else:
+                sheet_logger[f"{id}:database"] = "Id is not associated with any record in the database."
+
+        print(f"Process is {round(last_index/len(proposal_query_ids) * 100)}% complete")
+
+    self.append_process_logs(SHEET_NAME, {
+        "populate_template_department": {
+            "description": "Process populates the 'Admin Unit' column of the awards in the template document with the value the record is assigned in the Microsoft Access database. Additionally, the process also catches various types of issues found in the data from multiple sources such as the template file and the database and logs them.",
             "logs": sheet_logger
         }
     })

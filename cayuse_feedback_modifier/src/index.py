e598217b8d35457944ee8f2907b789dac687442b
Rey Flores <69604218+BluR3Y@users.noreply.github.com>
2024-10-29 20:31:11 -0400

    Implemented cell comment functionality

--- cayuse_feedback_modifier/src/index.py
+++ cayuse_feedback_modifier/src/index.py
@@ -2,11 +2,13 @@ from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv("../env/.env.development")
 
+import openpyxl.comments
 import pandas as pd
 import os
 import json
 import argparse
 import datetime
+import openpyxl
 
 import configs.db_config as db_config
 import configs.openai_config as openai_config
@@ -23,6 +25,7 @@ class FeedBackModifier:
         # Read Excel file
         self.df = pd.read_excel(self.filepath, sheet_name=None)
 
+        self.comment_cache = dict()
         self.logger = {
             'filename': os.path.basename(self.filepath),
             'timestamp': str(datetime.date.today()),
@@ -34,12 +37,26 @@ class FeedBackModifier:
         try:
             save_path = os.getenv('COPY_SAVE_PATH') if as_copy and os.getenv('COPY_SAVE_PATH') else os.path.dirname(self.filepath)
             file_name = f"{os.path.splitext(os.path.basename(self.filepath))[0]}{f" - modified_copy_{datetime.datetime.today().strftime('%m-%d-%Y')}" if as_copy else ''}.xlsx"
+            full_path = os.path.join(save_path, file_name)
 
             # Use ExcelWriter to write multiple sheets back into the Excel file
-            with pd.ExcelWriter(os.path.join(save_path, file_name), engine='openpyxl', mode='w') as writer:
+            with pd.ExcelWriter(full_path, engine='openpyxl', mode='w') as writer:
                 for sheet_name, df_sheet in self.df.items():
                     # Leave index as False if you don't want to save the index column from the DataFrame
                     df_sheet.to_excel(writer, sheet_name=sheet_name, index=index)
+
+            # Load the workbook
+            workbook = openpyxl.load_workbook(full_path)
+            for sheet_name in self.comment_cache:
+                if sheet_name in workbook.sheetnames:
+                    sheet_content = workbook[sheet_name]
+                    for cell_position in self.comment_cache[sheet_name]:
+                        cell = sheet_content[cell_position]
+                        cell.comment = openpyxl.comments.Comment(self.comment_cache[sheet_name][cell_position], "Developer")
+                else:
+                    raise Exception(f"The sheet with the name '{sheet_name}' does not exist in the workbook")
+            # Save the workbook with comments
+            workbook.save(full_path)
 
             # # Log the modifications
             with open(save_path + f"/excel_modifier_logger_{datetime.datetime.today().strftime('%m-%d-%Y')}.json", 'w') as json_file:
@@ -55,6 +72,13 @@ class FeedBackModifier:
         # Else, add the properties of the sheet to the class logger
         else:
             self.logger['sheets'][sheet].update(logs)
+
+    # Add cell comments to the object's comment cache
+    def append_cell_comments(self, sheet, cell, comment):
+        if sheet not in self.comment_cache:
+            self.comment_cache[sheet] = { cell: comment }
+        else:
+            self.comment_cache[sheet].update({ cell: comment })
         
 FeedBackModifier.execute_query = db_config.execute_query
 FeedBackModifier.modify_username = members.modify_entries
@@ -87,7 +111,6 @@ if __name__ == "__main__":
                 getattr(my_instance, method)()
             else:
                 raise Exception(f"The method {method} does not exist")
-        
         # Save document changes
         my_instance.save_excel_changes()
 

9de07a4885ed75291edcbb7b146abdf329e2b1fb
Rey Flores <69604218+BluR3Y@users.noreply.github.com>
2024-11-04 03:52:43 -0500

    Major modifications
    Created class for processes, allows for attaching data regarding processes, i.e, name, description, etc. Additionally, made modifications to logger.

--- cayuse_feedback_modifier/src/index.py
+++ cayuse_feedback_modifier/src/index.py
@@ -2,43 +2,64 @@ from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv("../env/.env.development")
 
-import openpyxl.comments
 import pandas as pd
 import os
 import json
 import argparse
 import datetime
-import openpyxl
+import inspect
 
 import configs.db_config as db_config
 import configs.openai_config as openai_config
-import sheets.members as members
-import sheets.attachments as attachments
+# import sheets.attachments as attachments
+# import sheets.members as members
 import sheets.awards as awards
 import sheets.proposals as proposals
-import utils
+from methods import utils
+from methods import logger
+from methods import comment
 
 class FeedBackModifier:
-    
+    # Variable that will store comments before creating them in the excel file
+    comment_cache = dict()
+
     def __init__(self):
-        # Access excel file path from environment variable
-        self.filepath = os.getenv('EXCEL_FILE_PATH')
         # Read Excel file
-        self.df = pd.read_excel(self.filepath, sheet_name=None)
+        self.df = pd.read_excel(os.getenv('EXCEL_FILE_PATH'), sheet_name=None)
+        # Store the sheet names
+        self.sheets = list(self.df.keys())
 
-        self.comment_cache = dict()
-        self.logger = {
-            'filename': os.path.basename(self.filepath),
-            'timestamp': str(datetime.date.today()),
-            'sheets': dict()
-        }
+        # Variable that will store the path of the json file that will store the logs
+        self.logs_path = os.path.join(os.getenv('SAVE_PATH') or os.path.dirname(self.filepath), 'cayuse_data_migration_logs.json')
+        print(self.logs_path)
+        # If the file exists:
+        if os.path.exists(self.logs_path):
+            # Store the logs in a variable
+            with open(self.logs_path) as json_file:
+                self.logs = json.load(json_file)
+        else:
+            # Initialize an empty dictionary
+            self.logs = dict()
 
-    # Save changes to excel file
-    def save_excel_changes(self, as_copy = True, index=False):
+        all_processes = dict()
+        for sheet_methods in [awards, proposals]:
+            if sheet_methods.SHEET_NAME in self.sheets:
+                sheet_processes = dict()
+                for fnName, fn in inspect.getmembers(sheet_methods, inspect.isfunction):
+                    process = fn(self)
+                    sheet_processes[process.name] = process
+                all_processes[sheet_methods.SHEET_NAME] = sheet_processes
+            else:
+                raise Exception(f"The sheet '{sheet_methods.SHEET_NAME}' does not exist in the workbook")
+        self.processes = all_processes
+
+    # Save changes to all related resources
+    def save_changes(self, as_copy = True, index=False):
         try:
-            save_path = os.getenv('COPY_SAVE_PATH') if as_copy and os.getenv('COPY_SAVE_PATH') else os.path.dirname(self.filepath)
-            file_name = f"{os.path.splitext(os.path.basename(self.filepath))[0]}{f" - modified_copy_{datetime.datetime.today().strftime('%m-%d-%Y')}" if as_copy else ''}.xlsx"
-            full_path = os.path.join(save_path, file_name)
+            excel_file_path = os.getenv('EXCEL_FILE_PATH')
+            file_save_path = os.getenv('SAVE_PATH') if as_copy and os.getenv('SAVE_PATH') else os.path.dirname(excel_file_path)
+            file_name = f"{os.path.splitext(os.path.basename(excel_file_path))[0]}{f" - modified_copy_{datetime.datetime.today().strftime('%m-%d-%Y')}" if as_copy else ''}.xlsx"
+            full_path = os.path.join(file_save_path, file_name)
 
             # Use ExcelWriter to write multiple sheets back into the Excel file
             with pd.ExcelWriter(full_path, engine='openpyxl', mode='w') as writer:
@@ -46,77 +67,45 @@ class FeedBackModifier:
                     # Leave index as False if you don't want to save the index column from the DataFrame
                     df_sheet.to_excel(writer, sheet_name=sheet_name, index=index)
 
-            # Load the workbook
-            workbook = openpyxl.load_workbook(full_path)
-            for sheet_name in self.comment_cache:
-                if sheet_name in workbook.sheetnames:
-                    sheet_content = workbook[sheet_name]
-                    for cell_position in self.comment_cache[sheet_name]:
-                        row, col = cell_position.split(':')
-                        cell = sheet_content.cell(int(row) + 1, int(col) + 1)   # Plus 1 accounts for rows and columns being 1-based index
-                        comment = openpyxl.comments.Comment(self.comment_cache[sheet_name][cell_position], "Developer")
-                        cell.comment = comment
-                        comment.height = 150 # Height in pixels
-                        comment.width = 300 # Width in pixels
-                else:
-                    raise Exception(f"The sheet with the name '{sheet_name}' does not exist in the workbook")
-            # Save the workbook with comments
-            workbook.save(full_path)
+            # Create cell comments in excel file
+            self.create_comments(full_path)
+            # Save the changes made to the logger
+            self.save_logs()
 
-            # # Log the modifications
-            with open(save_path + f"/excel_modifier_logger_{datetime.datetime.today().strftime('%m-%d-%Y')}.json", 'w') as json_file:
-                json.dump(self.logger, json_file)
         except Exception as e:
-            print(f"Error saving Excel file: {e}")
+            print(f"Error occured while saving changes: {e}")
 
-    # Adds process logs to the object's logger
-    def append_process_logs(self, sheet, logs):
-        # If no prior logs have been created for the provided sheet, initialize the property in the logger
-        if sheet not in self.logger['sheets']:
-            self.logger['sheets'][sheet] = logs
-        # Else, add the properties of the sheet to the class logger
-        else:
-            self.logger['sheets'][sheet].update(logs)
-
-    # Add cell comments to the object's comment cache
-    def append_cell_comment(self, sheet, row, col, comment):
-        if sheet not in self.comment_cache:
-            self.comment_cache[sheet] = { f"{row}:{col}": comment }
-        else:
-            self.comment_cache[sheet].update({ f"{row}:{col}": comment })
-        
+# Database related methods
 FeedBackModifier.execute_query = db_config.execute_query
-FeedBackModifier.modify_username = members.modify_entries
-FeedBackModifier.verify_entries = attachments.verify_entries
-FeedBackModifier.get_missing_project_attachments = attachments.missing_project_attachments
-FeedBackModifier.get_project_info = attachments.retrieve_project_info
-FeedBackModifier.populate_db_award_discipline = awards.populate_db_discipline
-FeedBackModifier.populate_template_award_discipline = awards.populate_template_discipline
-FeedBackModifier.populate_template_proposal_discipline = proposals.populate_template_discipline
-FeedBackModifier.populate_template_award_department = awards.populate_template_department
-FeedBackModifier.populate_template_proposal_department = proposals.populate_template_department
-FeedBackModifier.grant_abondant_record_field_modifier = utils.grant_abondant_record_field_modifier
 
-# Testing the Modifier class:
+# Logger related methods
+FeedBackModifier.append_logs = logger.append_logs
+FeedBackModifier.save_logs = logger.save_logs
+
+# Comment related methods
+FeedBackModifier.append_comment = comment.append_comment
+FeedBackModifier.create_comments = comment.create_comments
+
+# Run the program
 if __name__ == "__main__":
-    ## Create a new class instance
+    # Create a class instance
     my_instance = FeedBackModifier()
 
     # Initialize the argument parser
     parser = argparse.ArgumentParser(description="Parser handles command-line flags.")
 
     # Add optional flags and arguments
-    # parser.add_argument('--save', '-s', type=str, help='Save changes to original file or as a copy')
-    parser.add_argument('--methods', '-m', nargs='+', help='List of methods to call.')
+    parser.add_argument('--sheet', '-s', type=str, help="The name of the workbook sheet that the process belongs to.")
+    parser.add_argument('--process', '-p', action="append", help='Add process to call.')
 
     # Parse the arguments
     args = parser.parse_args()
 
-    if args.methods:
-        for method in args.methods:
-            if hasattr(my_instance, method) and callable(getattr(my_instance, method)):
-                getattr(my_instance, method)()
-            else:
-                raise Exception(f"The method {method} does not exist")
-        # Save document changes
-        my_instance.save_excel_changes()
+    if args.sheet and args.process:
+        if args.sheet in my_instance.processes:
+            for method in args.process:
+                my_instance.processes[args.sheet][method].logic()
+            # Save changes
+            my_instance.save_changes()
+        else:
+            print("Invalid sheet argument")
---
+++ cayuse_feedback_modifier/src/process_class.py
@@ -0,0 +1,7 @@
+
+class Process:
+    
+    def __init__(self, logic, name, description):
+        self.logic = logic
+        self.name = name
+        self.description = description

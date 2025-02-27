adccbe0c2053e5a187c4f7a9b7c9a000d886468c
Rey Flores <69604218+BluR3Y@users.noreply.github.com>
2025-02-26 21:23:34 -0500

    Modified attachment handler

--- src/packages/migration_manager/manage_migration.py
+++ src/packages/migration_manager/manage_migration.py
@@ -12,14 +12,14 @@ def manage_migration(db_path: str):
             
             query_grant_ids = db_manager.select_query(
                 table="grants",
-                cols=["Grant_ID"],
-                limit=200
+                cols=["Grant_ID"]
             )
             
-            # Toggle between T and F
-            append_existing = True
+            # Append Existing grants
+            append_existing = False
+            # Append Non-Existing grants
             append_new = False
-            appending_grants = [95394, 95548, 95541, 95435]
+            appending_grants = []
             for grant in query_grant_ids:
                 grant_id = grant['Grant_ID']
                 exists = grant_id in existing_grants
@@ -30,8 +30,10 @@ def manage_migration(db_path: str):
                     continue
                 
                 appending_grants.append(grant_id)
+            
 
-            for grant_id in appending_grants:                
+            for grant_id in appending_grants:
+                # Retrieve primary grant data
                 select_grant_query = db_manager.select_query(
                     table="grants",
                     conditions={
@@ -41,93 +43,119 @@ def manage_migration(db_path: str):
                         }
                     }
                 )[0]
-                # try:
-                #     migration_manager.projects_sheet_append(select_grant_query)
-                # except Exception as err:
-                #     print(f"Error while adding to projects sheet: {err}")
                 
-                # select_pi_query = db_manager.select_query(
-                #     table="PI_name",
-                #     conditions={
-                #         "PI_Grant_ID": {
-                #             "operator": "=",
-                #             "value": grant_id
-                #         }
-                #     }
-                # )
-                # try:
-                #     migration_manager.members_sheet_append(select_grant_query, select_pi_query)
-                # except Exception as err:
-                #     print(f"Error while adding to memebers sheet: {err}")
+                # Append grant to "projects" sheet
+                try:
+                    migration_manager.projects_sheet_append(select_grant_query)
+                except Exception as err:
+                    print(f"Error while adding to projects sheet: {err}")
                 
-                # select_total_query = db_manager.select_query(
-                #     table="total",
-                #     conditions={
-                #         "RFunds_Grant_ID": {
-                #             "operator": "=",
-                #             "value": grant_id
-                #         }
-                #     }
-                # )
-                # select_ri_funds_query = db_manager.select_query(
-                #     table="RIfunds",
-                #     conditions={
-                #         "RIFunds_Grant_ID": {
-                #             "operator": "=",
-                #             "value": grant_id
-                #         }
-                #     }
-                # )
-                # try:
-                #     migration_manager.proposals_sheet_append(select_grant_query, select_total_query, select_ri_funds_query)
-                # except Exception as err:
-                #     print(f"Error while adding to proposals sheet: {err}")
+                # Retrieve "primary investigator" records relating to grant
+                select_pi_query = db_manager.select_query(
+                    table="PI_name",
+                    conditions={
+                        "PI_Grant_ID": {
+                            "operator": "=",
+                            "value": grant_id
+                        }
+                    }
+                )
+                # Append grant to "members" sheet
+                try:
+                    migration_manager.members_sheet_append(select_grant_query, select_pi_query)
+                except Exception as err:
+                    print(f"Error while adding to memebers sheet: {err}")
                 
-                # select_dates_query = db_manager.select_query(
-                #     table="Dates",
-                #     conditions={
-                #         "Date_GrantID": {
-                #             "operator": "=",
-                #             "value": grant_id
-                #         }
-                #     }
-                # )
-                # select_dates_query = (select_dates_query[0] if len(select_dates_query) else {})
-                # select_costshare_query = db_manager.select_query(
-                #     table="CostShare",
-                #     conditions={
-                #         "GrantID": {
-                #             "operator": "=",
-                #             "value": grant_id
-                #         }
-                #     }
-                # )
-                # select_ffunds_query = db_manager.select_query(
-                #     table="Ffunds",
-                #     conditions={
-                #         "FFunds_Grant_ID": {
-                #             "operator": "=",
-                #             "value": grant_id
-                #         }
-                #     }
-                # )
-                # select_fifunds_query = db_manager.select_query(
-                #     table="FIFunds",
-                #     conditions={
-                #         "FIFunds_Grant_ID": {
-                #             "operator": "=",
-                #             "value": grant_id
-                #         }
-                #     }
-                # )
-                # if select_grant_query['Status'] == "Funded" or select_grant_query['RF_Account'] != None:
-                #     try:
-                #         migration_manager.awards_sheet_append(select_grant_query, select_dates_query, select_total_query, select_ri_funds_query, select_costshare_query, select_ffunds_query, select_fifunds_query)
-                #     except Exception as err:
-                #         print(f"Error occured while adding to awards sheet: {err}")
+                # Retrieve "financial" records relating to grant
+                select_total_query = db_manager.select_query(
+                    table="total",
+                    conditions={
+                        "RFunds_Grant_ID": {
+                            "operator": "=",
+                            "value": grant_id
+                        }
+                    }
+                )
+                # Retrieve "ri funds" records relating to grant
+                select_ri_funds_query = db_manager.select_query(
+                    table="RIfunds",
+                    conditions={
+                        "RIFunds_Grant_ID": {
+                            "operator": "=",
+                            "value": grant_id
+                        }
+                    }
+                )
                 
+                # Append grant to "proposals" sheet
+                try:
+                    migration_manager.proposals_sheet_append(select_grant_query, select_total_query, select_ri_funds_query)
+                except Exception as err:
+                    print(f"Error while adding to proposals sheet: {err}")
+                
+                # Retrieve "updates" records relating to grant
+                select_dates_query = db_manager.select_query(
+                    table="Dates",
+                    conditions={
+                        "Date_GrantID": {
+                            "operator": "=",
+                            "value": grant_id
+                        }
+                    }
+                )
+                select_dates_query = (select_dates_query[0] if len(select_dates_query) else {})
+                
+                # Retrieve "constshare" records relating to grant
+                select_costshare_query = db_manager.select_query(
+                    table="CostShare",
+                    conditions={
+                        "GrantID": {
+                            "operator": "=",
+                            "value": grant_id
+                        }
+                    }
+                )
+                
+                # Retrieve "f funds" records relating to grant
+                select_ffunds_query = db_manager.select_query(
+                    table="Ffunds",
+                    conditions={
+                        "FFunds_Grant_ID": {
+                            "operator": "=",
+                            "value": grant_id
+                        }
+                    }
+                )
+                
+                # Retrieve "fi funds" relating to grant
+                select_fifunds_query = db_manager.select_query(
+                    table="FIFunds",
+                    conditions={
+                        "FIFunds_Grant_ID": {
+                            "operator": "=",
+                            "value": grant_id
+                        }
+                    }
+                )
+                
+                # If grant is funded or has an 'RF_Account' number, all funded grants are assigned one:
                 if select_grant_query['Status'] == "Funded" or select_grant_query['RF_Account'] != None:
+                    # Append grant to "awards" sheet
+                    try:
+                        migration_manager.awards_sheet_append(
+                            select_grant_query,
+                            select_dates_query,
+                            select_total_query,
+                            select_ri_funds_query,
+                            select_costshare_query,
+                            select_ffunds_query,
+                            select_fifunds_query
+                        )
+                    except Exception as err:
+                        print(f"Error occured while appending to awards sheet: {err}")
+                    
+                    # Append grant to "attachments" sheet
                     try:
                         migration_manager.attachments_sheet_append(select_grant_query)
                     except Exception as err:
-                        print(f"Error occured while adding grant {grant_id} to attachments sheet: {err}")
+                        print(f"Error occured while appending to attachments sheet: {err}")
--- src/packages/migration_manager/sheets/attachments.py
+++ src/packages/migration_manager/sheets/attachments.py
@@ -23,6 +23,8 @@ def find_closest_file(base_dir, file_path):
     if not base_dir_obj.exists() or not base_dir_obj.is_dir():
         return None  # Base directory must exist
 
+    relative_path = Path()  # Stores the closest matching relative path
+
     for part in file_path_obj.parts:
 
         # Get list of directories and files in the current base directory
@@ -32,9 +34,11 @@ def find_closest_file(base_dir, file_path):
         if not closest_item:
             return None  # No close match found
 
+        relative_path /= closest_item   # Build the relative closest path
         base_dir_obj /= closest_item  # Move deeper into the structure
 
-    return base_dir_obj if base_dir_obj.exists() else None  # Return closest match
+    # return base_dir_obj if base_dir_obj.exists() else None  # Return closest match
+    return relative_path  # Return only the relative closest path
 
 def attachments_sheet_append(self: "MigrationManager", grant_data: dict):
     gt_manager = self.generated_template_manager
@@ -43,35 +47,34 @@ def attachments_sheet_append(self: "MigrationManager", grant_data: dict):
     
     grant_id = grant_data['Grant_ID']
     grant_pln = grant_data['Project_Legacy_Number']
-    # Do not leave empty
-    dir_path = ""
+    dir_path = os.getenv("PROPOSAL_PATH")
     
-    proposal_attachments = ft_manager.get_entries(
+    grant_attachments = ft_manager.get_entries(
         SHEET_NAME, {
-            "projectLegacyNumber": grant_pln,
-            "form": "Proposal"
+            "projectLegacyNumber": grant_pln
         },
         return_all=True
     )
-    if proposal_attachments:
-        for attachment in  proposal_attachments:
+    if grant_attachments:
+        for attachment in grant_attachments:
             file_path = attachment['filePath']
+            attachment_form = attachment['form']
             
             if file_path:
                 if not validate_file_path(os.path.join(dir_path, file_path)):
                     closest_matching_file = find_closest_file(dir_path, file_path)
                     if closest_matching_file:
                         file_path = closest_matching_file
-                        gt_manager.property_manager.append_comment(SHEET_NAME, next_row, 7, 'notice', "Stored file path was invalid but matched a similar one.")
+                        gt_manager.property_manager.append_comment(SHEET_NAME, next_row, 7, 'notice', f"Path '{file_path}' was invalid but matched a similar one.")
                     else:
                         gt_manager.property_manager.append_comment(SHEET_NAME, next_row, 7, 'error', "Path does not point to an existing file.")
             else:
                 gt_manager.property_manager.append_comment(SHEET_NAME, next_row, 7, 'error', "Attachment does not include a file path.")
-                
+            
             gt_manager.append_row(SHEET_NAME, {
                 "projectLegacyNumber": grant_pln,
-                "form": "Proposal",
-                "legacyNumber": grant_id,
+                "form": attachment_form,    # Subawards -> Proposals
+                "legacyNumber": str(grant_id) + ("-award" if attachment_form == "Award" else ''),
                 "filePath": file_path
             })
             next_row += 1
@@ -81,44 +84,5 @@ def attachments_sheet_append(self: "MigrationManager", grant_data: dict):
             "form": "Proposal",
             "legacyNumber": f"{grant_id}"
         })
-        gt_manager.property_manager.append_comment(SHEET_NAME, next_row, 0, 'error', "Grant is missing 'Proposal' Attachments.")
-        next_row += 1
-    
-    award_attachments = ft_manager.get_entries(
-        SHEET_NAME, {
-            "projectLegacyNumber": grant_pln,
-            "form": "Award"
-        },
-        return_all=True
-    ) 
-
-    if award_attachments:
-        for attachment in award_attachments:
-            file_path = attachment['filePath']
-            
-            if file_path:
-                if not validate_file_path(os.path.join(dir_path, file_path)):
-                    closest_matching_file = find_closest_file(dir_path, file_path)
-                    if closest_matching_file:
-                        file_path = closest_matching_file
-                        gt_manager.property_manager.append_comment(SHEET_NAME, next_row, 7, 'notice', "Stored file path was invalid but matched a similar one.")
-                    else:
-                        gt_manager.property_manager.append_comment(SHEET_NAME, next_row, 7, 'error', "Path does not point to an existing file.")
-            else:
-                gt_manager.property_manager.append_comment(SHEET_NAME, next_row, 7, 'error', "Attachment does not include a file path.")
-                
-            gt_manager.append_row(SHEET_NAME, {
-                "projectLegacyNumber": grant_pln,
-                "form": "Award",
-                "legacyNumber": f"{grant_id}-award",
-                "filePath": file_path
-            })
-            next_row += 1
-    else:
-        gt_manager.append_row(SHEET_NAME, {
-            "projectLegacyNumber": grant_pln,
-            "form": "Award",
-            "legacyNumber": f"{grant_id}-award"
-        })
-        gt_manager.property_manager.append_comment(SHEET_NAME, next_row, 0, 'error', "Grant is missing 'Award' Attachments.")
+        gt_manager.property_manager.append_comment(SHEET_NAME, next_row, 0, 'error', "Grant is missing Attachments.")
         next_row += 1
